96630efcdbbf7207d2525555af78bd826f2f4b0d
Jakub Solecki <jakub.solecki@onet.com.pl>
2020-11-05 14:02:00 +0100

    lab4 done

--- lab4/src/pl/jakubsolecki/task2/Main.java
+++ lab4/src/pl/jakubsolecki/task2/Main.java
@@ -4,11 +4,11 @@ import pl.jakubsolecki.task1.Processor;
 
 public class Main {
     public static void main(String[] args) {
-        final int M = 1000;
-        final Buffer buffer = new NaiveBuffer(M);
-//        final Buffer buffer = new FairBuffer(M);
+        final int M = 100000;
+//        final Buffer buffer = new NaiveBuffer(M);
+        final Buffer buffer = new FairBuffer(M);
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 1000; i++) {
             (new Producer(buffer)).start();
             (new Consumer(buffer)).start();
         }
