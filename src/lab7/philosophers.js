05f4ba46eb0c2c667a68f52c7a87fa935fe379b8
Jakub Solecki <jakub.solecki@onet.com.pl>
2020-12-03 00:49:59 +0100

    conductor works, i guess

--- src/lab7/philosophers.js
+++ src/lab7/philosophers.js
@@ -6,8 +6,8 @@ var Fork = function() {
 Fork.prototype.acquire = function(cb) {
     let waitTime = 1;
     const takeFork = () => {
-        if (this.state == 1) {
-            waitTime *= 2;
+        if (this.state == 1) { // BEB
+            waitTime *= 2;  
             setTimeout(takeFork, waitTime);
         } else {
             this.state = 1;
@@ -73,48 +73,45 @@ Philosopher.prototype.startAsym = function(count) {
         f1 = id % 2 == 0 ? this.f1 : this.f2,
         f2 = id % 2 == 0 ? this.f2 : this.f1;
 
-    // const eat = (id, count) => {
-    //     printForks();
-    //     forks[f1].acquire(() => {
-    //         printForks();
-    //         forks[f2].acquire(() => {
-    //             printForks();
-    //             setTimeout(() => {
-    //                 printForks();
-    //                 forks[f1].release(() => {
-    //                     printForks();
-    //                     forks[f2].release(() => {
-    //                         printForks();
-    //                         count--;
-    //                         if (count > 0) {
-    //                             setTimeout(() => eat(id, count));
-    //                         }
-    //                     });
-    //                 });
-    //             }, 2);
-    //         });
-    //     });
-    // }
+        const eat = (id, count) => {
+            printForks();
+            forks[f1].acquire(() => {
+                printForks();
+                forks[f2].acquire(() => {
+                    printForks();
+                    setTimeout(() => {
+                        printForks();
+                        forks[f1].release(() => {
+                            printForks();
+                            forks[f2].release(() => {
+                                printForks();
+                                count--;
+                                if (count > 0) {
+                                    setTimeout(() => eat(id, count));
+                                }
+                            });
+                        });
+                    }, 2);
+                });
+            });
+        }
 
-    // setTimeout(() => eat(id, count), 0);
-    this.startNaive(count);
+        setTimeout(() => eat(id, count), 0);
 };
 
 Philosopher.prototype.startConductor = function(count) {
-    let forks = this.forks,
-        f1 = this.f1,
-        f2 = this.f2,
-        id = this.id;
+
+    if (count > 0) {
+        conductor.requestForks(this, count);
+    }
 };
 
 Philosopher.prototype.giveForks = function(count) {
-    let philosopher = this;
-    setTimeout(() => conductor.release(this, count), 2);
+    setTimeout(() => conductor.returnForks(this, count), 2);
 }
 
 var Conductor = function() {
-    // this.state = 0;
-    this = philosophersCount = 0;
+    this.philosophersCounter = 0;
     return this;
 }
 
@@ -124,17 +121,30 @@ Conductor.prototype.requestForks = function(philosopher, count) {
         f1 = philosopher.f1,
         f2 = philosopher.f2;
 
-    if (forks[f1].state === 0 && forks[f2].state === 0) {
+        printForks();
+    if (forks[f1].state === 0 && forks[f2].state === 0 && this.philosophersCounter < N) {
         forks[f1].state = 1;
-        forks[f2].state = 1;    
+        forks[f2].state = 1;   
+        this.philosophersCounter += 1; 
+        printForks();
         philosopher.giveForks(count);
     } else {
-        // TODO
+        setTimeout(() => this.requestForks(philosopher, count), 2);
     }
 }
 
 Conductor.prototype.returnForks = function(philosopher, count) {
     // TODO
+    let f1 = philosopher.f1,
+        f2 = philosopher.f2,
+        forks = philosopher.forks,
+        id = philosopher.id;
+
+    forks[f1].state = 0;
+    forks[f2].state = 0;
+    this.philosophersCounter -= 1;
+    philosopher.startConductor(count - 1);
+
 }
 
 let N = 5;
@@ -152,6 +162,6 @@ for (let i = 0; i < N; i++) {
 
 for (let i = 0; i < N; i++) {
     // philosophers[i].startNaive(2000);
-    philosophers[i].startAsym(10);
-    // philosophers[i].startNaive(2000);
+    // philosophers[i].startAsym(10);
+    philosophers[i].startConductor(10);
 }
