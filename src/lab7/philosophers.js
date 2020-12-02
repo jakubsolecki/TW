c85c9ea97dc7dccba42b22f3778f549bddf43b42
Jakub Solecki <jakub.solecki@onet.com.pl>
2020-12-02 14:27:48 +0100

    conductor WIP

--- src/lab7/philosophers.js
+++ src/lab7/philosophers.js
@@ -73,42 +73,75 @@ Philosopher.prototype.startAsym = function(count) {
         f1 = id % 2 == 0 ? this.f1 : this.f2,
         f2 = id % 2 == 0 ? this.f2 : this.f1;
 
-    const eat = (id, count) => {
-        printForks();
-        forks[f1].acquire(() => {
-            printForks();
-            forks[f2].acquire(() => {
-                printForks();
-                setTimeout(() => {
-                    printForks();
-                    forks[f1].release(() => {
-                        printForks();
-                        forks[f2].release(() => {
-                            printForks();
-                            count--;
-                            if (count > 0) {
-                                setTimeout(() => eat(id, count));
-                            }
-                        });
-                    });
-                }, 2);
-            });
-        });
-    }
+    // const eat = (id, count) => {
+    //     printForks();
+    //     forks[f1].acquire(() => {
+    //         printForks();
+    //         forks[f2].acquire(() => {
+    //             printForks();
+    //             setTimeout(() => {
+    //                 printForks();
+    //                 forks[f1].release(() => {
+    //                     printForks();
+    //                     forks[f2].release(() => {
+    //                         printForks();
+    //                         count--;
+    //                         if (count > 0) {
+    //                             setTimeout(() => eat(id, count));
+    //                         }
+    //                     });
+    //                 });
+    //             }, 2);
+    //         });
+    //     });
+    // }
 
-    setTimeout(() => eat(id, count), 0);
+    // setTimeout(() => eat(id, count), 0);
+    this.startNaive(count);
 };
 
 Philosopher.prototype.startConductor = function(count) {
     let forks = this.forks,
-    f1 = this.f1,
-    f2 = this.f2,
-    id = this.id;
+        f1 = this.f1,
+        f2 = this.f2,
+        id = this.id;
 };
+
+Philosopher.prototype.giveForks = function(count) {
+    let philosopher = this;
+    setTimeout(() => conductor.release(this, count), 2);
+}
+
+var Conductor = function() {
+    // this.state = 0;
+    this = philosophersCount = 0;
+    return this;
+}
+
+Conductor.prototype.requestForks = function(philosopher, count) {
+    let forks = philosopher.forks,
+        id = philosopher.id,
+        f1 = philosopher.f1,
+        f2 = philosopher.f2;
+
+    if (forks[f1].state === 0 && forks[f2].state === 0) {
+        forks[f1].state = 1;
+        forks[f2].state = 1;    
+        philosopher.giveForks(count);
+    } else {
+        // TODO
+    }
+}
+
+Conductor.prototype.returnForks = function(philosopher, count) {
+    // TODO
+}
 
 let N = 5;
 let forks = [];
-let philosophers = []
+let philosophers = [];
+let conductor = new Conductor();
+
 for (let i = 0; i < N; i++) {
     forks.push(new Fork());
 }
