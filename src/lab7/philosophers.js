6ebb446a08f6ac8176d18ba750ee4b8737c0b0c9
Jakub Solecki <jakub.solecki@onet.com.pl>
2021-01-13 11:42:18 +0100

    lab7

--- src/lab7/philosophers.js
+++ src/lab7/philosophers.js
@@ -8,6 +8,7 @@ Fork.prototype.acquire = function(cb) {
     const takeFork = () => {
         if (this.state == 1) { // BEB
             waitTime *= 2;  
+            console.log(waitTime);
             setTimeout(takeFork, waitTime);
         } else {
             this.state = 1;
@@ -83,9 +84,9 @@ Philosopher.prototype.startAsym = function(count) {
             forks[f1].acquire(() => {
                 printForks();
                 forks[f2].acquire(() => {
+                    sumOfTimes += (Date.now() - startTime);
                     printForks();
                     setTimeout(() => {
-                        sumOfTimes += (Date.now() - startTime);
                         printForks();
                         forks[f1].release(() => {
                             printForks();
@@ -93,12 +94,12 @@ Philosopher.prototype.startAsym = function(count) {
                                 printForks();
                                 count--;
                                 if (count > 0) {
-                                    setTimeout(() => eat(id, count));
+                                    setTimeout(() => eat(id, count), 2);
                                 }
                             });
                         });
-                    }, 2);
-                });
+                    });
+                }, 2);
             });
         }
 
@@ -115,9 +116,9 @@ Philosopher.prototype.startConductor = function(count) {
     }
 };
 
-Philosopher.prototype.giveForks = function(count) {
-    setTimeout(() => conductor.returnForks(this, count), 2);
-}
+// Philosopher.prototype.giveForks = function(count) {
+//     setTimeout(() => conductor.returnForks(this, count), 2);
+// }
 
 var Conductor = function() {
     this.philosophersCounter = 0;
@@ -130,21 +131,63 @@ Conductor.prototype.requestForks = function(philosopher, count, startTime) {
         f1 = philosopher.f1,
         f2 = philosopher.f2;
 
+    let waitTime = 1;
+
+    //     // printForks();
+    // const tryTakingForks = () => {
+    //     if (forks[f1].state === 0 && forks[f2].state === 0 && this.philosophersCounter < N) {
+    //         sumOfTimes += (Date.now() - startTime);
+    //         forks[f1].state = 1;
+    //         forks[f2].state = 1;   
+    //         this.philosophersCounter += 1; 
+    //         printForks();
+    //         philosopher.giveForks(count);
+    //     } else {
+    //         waitTime *= 2;
+    //         setTimeout(tryTakingForks, waitTime);
+    //     }
+    // }
+
+    // tryTakingForks();
+
+    const eat = (id, count) => {
         printForks();
-    if (forks[f1].state === 0 && forks[f2].state === 0 && this.philosophersCounter < N) {
-        sumOfTimes += (Date.now() - startTime);
-        forks[f1].state = 1;
-        forks[f2].state = 1;   
-        this.philosophersCounter += 1; 
-        printForks();
-        philosopher.giveForks(count);
-    } else {
-        setTimeout(() => this.requestForks(philosopher, count, startTime), 2);
+        startTime = Date.now();
+        forks[f1].acquire(() => {
+            printForks();
+            forks[f2].acquire(() => {
+                sumOfTimes += (Date.now() - startTime);
+                printForks();
+                setTimeout(() => {
+                    printForks();
+                    forks[f1].release(() => {
+                        printForks();
+                        forks[f2].release(() => {
+                            printForks();
+                            count--;
+                            if (count > 0) {
+                                this.philosophersCounter -= 1;
+                                setTimeout(() => philosopher.startConductor(count-1), 2);
+                            }
+                        });
+                    });
+                }, 2);
+            });
+        });
     }
+
+    if (this.philosophersCounter < 4) {
+        this.philosophersCounter += 1;
+        setTimeout(() => eat(id, count), 0);
+    } else {
+        waitTime *= 2;
+        setTimeout(() => this.requestForks(philosopher, count), 2*waitTime)
+    }
+    
+
 }
 
 Conductor.prototype.returnForks = function(philosopher, count) {
-    // TODO
     let f1 = philosopher.f1,
         f2 = philosopher.f2,
         forks = philosopher.forks,
@@ -152,12 +195,13 @@ Conductor.prototype.returnForks = function(philosopher, count) {
 
     forks[f1].state = 0;
     forks[f2].state = 0;
+    printForks();
     this.philosophersCounter -= 1;
     philosopher.startConductor(count - 1);
 
 }
 
-let N = 5;
+let N = 20;
 let forks = [];
 let philosophers = [];
 let conductor = new Conductor();
@@ -171,9 +215,9 @@ for (let i = 0; i < N; i++) {
 }
 
 for (let i = 0; i < N; i++) {
-    // philosophers[i].startNaive(2000);
-    // philosophers[i].startAsym(10);
-    philosophers[i].startConductor(100);
+    philosophers[i].startNaive(2000);
+    // philosophers[i].startAsym(100);
+    // philosophers[i].startConductor(100);
 }
 
-setTimeout(() => console.log(sumOfTimes), 1000);
+// setTimeout(() => console.log(sumOfTimes), 10000);
